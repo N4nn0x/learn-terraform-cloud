acb5dd92f2715e431ca3bca51ab48c275a7f9533
N4nn0x <119273133+N4nn0x@users.noreply.github.com>
2023-09-27 21:34:59 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,10 @@ required_providers {
 }
 
 provider "azurerm" {
-  feature {}
+   client_id       = var.ARM_CLIENT_ID
+  client_secret   = var.ARM_CLIENT_SECRET
+  subscription_id = var.ARM_SUBSCRIPTION_ID
+  tenant_id       = var.ARM_TENANT_ID
 }
 
 resource "azurerm_resource_group" "mtc-rg" {
