a20780ef03ba5b61d0c8aede9fadfe53486d004e
N4nn0x <119273133+N4nn0x@users.noreply.github.com>
2023-10-01 17:24:09 +1100

    Add files via upload

--- variables.tf
+++ variables.tf
@@ -1,15 +1,3 @@
-variable "ARM_CLIENT_ID" {
-  description = "Azure Client ID"
-}
-
-variable "ARM_CLIENT_SECRET" {
-  description = "Azure Client Secret"
-}
-
-variable "ARM_SUBSCRIPTION_ID" {
-  description = "Azure Subscription ID"
-}
-
-variable "ARM_TENANT_ID" {
-  description = "Azure Tenant ID"
+variable "host_os" {
+  type = string
 }
