9ec864f2f2befe68e6f20d1ecee785f93eca524d
N4nn0x <119273133+N4nn0x@users.noreply.github.com>
2023-10-25 15:24:52 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -209,7 +209,7 @@ resource "azurerm_linux_function_app" "mtc-functionapp" {
 
   storage_account_name       = azurerm_storage_account.mtc-sa.name
   storage_account_access_key = azurerm_storage_account.mtc-sa.primary_access_key
-  service_plan_id            = azurerm_service_plan.mtc-sp.id
+  #service_plan_id            = azurerm_service_plan.mtc-sp.id
 
 site_config {
 }
