976f38c2e85cd13f909559ccf4b9987bc6003fd7
N4nn0x <119273133+N4nn0x@users.noreply.github.com>
2023-10-25 12:28:29 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "azurerm_resource_group" "mtc-rg" {
     environment = "Dev"
   }
 }
-/*
+
 resource "azurerm_virtual_network" "mtc-vn" {
   name                = "mtc-network"
   resource_group_name = azurerm_resource_group.mtc-rg.name
@@ -230,16 +230,3 @@ app_settings = {
   }
 }
 
-*/
-
-
-
-
-
-
-
-
-
-
-
-*/
