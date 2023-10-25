49d6a91dd40d75d609a627e3dc0ffa036697ee9c
N4nn0x <119273133+N4nn0x@users.noreply.github.com>
2023-10-25 12:13:47 +1100

    Update PythonScript.py

--- AzureFunction/PythonScript.py
+++ AzureFunction/PythonScript.py
@@ -1,4 +1,30 @@
 import subprocess
+from flask import Flask, request, jsonify
+
+app = Flask(__name)
+
+# Define the Docker container name
+container_name = "nano"
+
+# Build the Docker stop command
+docker_stop_command = ["sudo", "docker", "stop", container_name]
+
+@app.route('/stop-container', methods=['POST'])
+def stop_container():
+    try:
+        # Run the Docker stop command
+        subprocess.run(docker_stop_command, check=True)
+        return jsonify({"message": f"Container '{container_name}' stopped successfully."})
+    except subprocess.CalledProcessError as e:
+        # Handle any errors, such as if the container doesn't exist
+        return jsonify({"error": f"Error: {e}"}), 500
+
+if __name__ == '__main__':
+    app.run()
+
+
+/*
+import subprocess
 
 # Define the Docker container name
 container_name = "nano"
@@ -13,3 +39,4 @@ try:
 except subprocess.CalledProcessError as e:
     # Handle any errors, such as if the container doesn't exist
     print(f"Error: {e}")
+*/
