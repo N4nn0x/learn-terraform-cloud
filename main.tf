9b79db3cd84db7ada1c28831b995956d37b20924
N4nn0x <119273133+N4nn0x@users.noreply.github.com>
2024-10-07 13:05:26 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -180,6 +180,6 @@ data "azurerm_public_ip" "mtc-ip-data" {
 
 output "public_ip_address" {
   value = "${azurerm_linux_virtual_machine.mtc-vm.name}: ${data.azurerm_public_ip.mtc-ip-data.ip_address}"
-}
+ }
 }
 */
